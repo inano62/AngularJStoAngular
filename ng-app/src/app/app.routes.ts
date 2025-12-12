e1e9371f6873c2e1c7e8b285cc421e71553d411b
inano62 <jemesouviens7@gmail.com>
2025-12-12 21:18:54 +0900

    iframeにての設定。

--- ng-app/src/app/app.routes.ts
+++ ng-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {V2Component} from './pages/v2-component/v2-component'
+import {HomeComponent} from './pages/home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component :HomeComponent},
+  { path: 'v2', component :V2Component},
+];
